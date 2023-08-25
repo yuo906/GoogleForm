e3376a4100e25c1ad1153febbd18b8f1b895bb70
tzuchi07 <140991232+tzuchi07@users.noreply.github.com>
2023-08-26 07:17:36 +0800

    question2

---
+++ question.js
@@ -0,0 +1,14 @@
+    console.log(123);
+    let li =document.querySelectorAll('.number0-1 li');
+    console.log(li);
+// console.log(li);
+        // li.forEach(item=> {item.addEventListener('click',()=>{
+        //     item.style.backgroundColor='red';
+        //     console.log(li[0]);
+        //     li[0].classList.remove('show-number');
+        //     choose0_1.classList.add('show-number');
+        //     li[1].classList.add('show-number');
+        //     li[2].classList.add('show-number');
+        //     console.log(li);
+        //     })  
+        // });
