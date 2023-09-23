299910a211a493fb562663f130d5860b2af03594
yuo906 <140991318+yuo906@users.noreply.github.com>
2023-09-23 17:04:19 +0800

    response完成

---
+++ js/response.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector('.button-circle');
+let responseBody = document.querySelector('.response-body');
+
+btn.addEventListener('click', function(){
+  // let buttonCircle = document.querySelector('.button-circle');
+  
+  btn.classList.toggle('active');
+  console.log(btn);
+})
